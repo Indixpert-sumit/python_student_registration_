c2ab13fe69c9a1bfb0e4ea05de231c6c78bbc14c
Indixpert-sumit <sy0050854@gmail.com>
2025-03-20 17:04:26 +0530

    taking 2 student details

---
+++ user_registration.py
@@ -0,0 +1,27 @@
+data1={}
+qualification={}
+listdata1=[]
+
+data1['student id :']=input('enter student id ')
+data1['name']=input('enter your name ')
+data1['experience']=input('enter your experience ')
+data1['skills']=input('enter your skills ')
+qualification['qualification']=2020,'b.c.a','passingyear',2024
+listdata1.append(qualification)
+
+
+data2={}
+qualification={}
+listdata2=[]
+
+data2['2nd student id :']=input('enter 2nd student id ')
+data2['name']=input('enter 2nd your name ')
+data2['experience']=input('enter your experience ')
+data2['skills']=input('enter your skills ')
+qualification['qualification']='10th''passing year',2020,
+listdata2.append(qualification)
+
+print(data1)
+print(listdata1)
+print(data2)
+print(listdata2)
